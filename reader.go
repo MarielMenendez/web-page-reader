4d0a6ba296cd7a4c5b7e07636842e74e3b4b0a13
MarielMenendez <mariel_menendez@hotmail.com>
2025-02-23 17:15:31 +0100

    Update reader.go

--- reader.go
+++ reader.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/mrusme/reader/cmd"
+import (
+    "log"
+    "net/http"
+)
 
 func main() {
-	cmd.Execute()
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Web Page Reader is running!"))
+    })
+
+    log.Println("Server started on :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
